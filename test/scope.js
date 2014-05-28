59bfe6fbb2cf6323182c2d30e267280d80a91265
Tyler Johnson <tyler@vintyge.com>
2014-05-28 16:28:52 -0600

    moved observers and reactivity to scopes from models

--- test/scope.js
+++ test/scope.js
@@ -49,61 +49,386 @@ describe("Scope", function() {
 		expect(scope.get("this.bar")).to.be.undefined;
 	});
 
-	it("stops observing", function() {
-		var fn = function() { throw new Error("Observer wasn't removed!"); }
-		scope.observe("foo", fn);
+	describe("#observe()", function() {
 		
-		expect(function() {
+		it("successfully adds observer", function() {
+			var fn = function(){};
+			scope.observe("foo", fn);
+			expect(scope._observers.some(function(o) {
+				return o.fn === fn;
+			})).to.be.ok;
+		});
+
+		it("successfully removes observer", function() {
+			var fn = function() { throw new Error("Observer wasn't removed!"); }
+			scope.observe("foo", fn);
+			
+			expect(function() {
+				scope.set("foo", "baz");
+			}).to.throw(Error);
+
+			scope.stopObserving("foo", fn);
+			scope.set("foo", "bar");
+		});
+
+		it("calling stopObserving() without arguments clears all observers", function() {
+			scope.observe("foo", function() { throw new Error("Observer wasn't removed!"); });
+			expect(scope._observers).to.have.length(1);
+			scope.stopObserving();
 			scope.set("foo", "baz");
-		}).to.throw(Error);
-
-		scope.stopObserving("foo", fn);
-		scope.set("foo", "bar");
-	});
-
-	it("observes changes to fallback value", function() {
-		var seen = false;
-
-		scope.observe("bar", function(summary) {
-			expect(this).to.equal(scope);
-			seen = true;
+			expect(scope._observers).to.have.length(0);
 		});
 
-		fallback.set("bar", "bam");
-		expect(seen).to.be.ok;
-	});
-
-	it("observes changes to local value when the value was found in fallback", function() {
-		var seen = false;
-
-		scope.observe("bar", function(summary) {
-			seen = true;
+		it("calling stopObserving(path) clears all observers with matching path", function() {
+			var fn = function() { throw new Error("Observer wasn't removed!"); }
+			scope.observe("foo", fn);
+			scope.observe("foo", fn);
+			scope.observe("bar", fn);
+			expect(scope._observers).to.have.length(3);
+			scope.stopObserving("foo");
+			scope.set("foo", "baz");
+			expect(scope._observers).to.have.length(1);
 		});
 
-		scope.set("bar", "bam");
-		expect(seen).to.be.ok;
-	});
-
-	it("doesn't observes changes to fallback when the value is in local model", function() {
-		var seen = false;
-
-		scope.observe("foo", function(summary) {
-			seen = true;
+		it("calling stopObserving(null, fn) clears all observers with matching function", function() {
+			var fn = function() { throw new Error("Observer wasn't removed!"); }
+			scope.observe("foo", fn);
+			scope.observe("bar", fn);
+			scope.observe("baz", function(){});
+			expect(scope._observers).to.have.length(3);
+			scope.stopObserving(null, fn);
+			scope.set("foo", "baz");
+			expect(scope._observers).to.have.length(1);
 		});
 
-		fallback.set("foo", "bam");
-		expect(seen).to.not.be.ok;
-	});
-
-	it("observes unsets on local value", function() {
-		var seen = false;
-
-		scope.observe("foo", function(summary) {
-			seen = true;
+		it("observes nothing when nothing changes", function() {
+			scope.observe("foo", function() { throw new Error("A change was observed."); });
+			scope.set("foo", "bar");
 		});
 
-		scope.unset("foo");
-		expect(seen).to.be.ok;
+		it("observes static path changes", function() {
+			var seen = false;
+			scope.observe("foo.bar", function(chg) {
+				expect(this).to.equal(scope);
+				
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo.bar"),
+					previousModel: scope.getModel("foo.bar"),
+					path: "foo.bar",
+					type: "add",
+					value: "baz",
+					oldValue: undefined
+				});
+
+				seen = true;
+			});
+
+			scope.set("foo", { bar: "baz" });
+			expect(seen).to.be.ok;
+		});
+
+		it("observes changes only once", function() {
+			var seen = 0;
+			scope.observe("foo", function() { seen++; });
+			scope.set("foo", { bar: "baz" });
+			expect(seen).to.equal(1);
+		});
+
+		it("observes unset", function() {
+			var seen = false;
+			scope.observe("foo", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo"),
+					previousModel: scope.getModel("foo"),
+					path: "foo",
+					type: "delete",
+					value: undefined,
+					oldValue: "bar"
+				});
+
+				seen = true;
+			});
+
+			scope.unset("foo");
+			expect(seen).to.be.ok;
+		});
+
+		it("calling get() in an observer returns the new value", function() {
+			var seen = false;
+			scope.observe("foo.bar", function(chg) {
+				expect(this.get(chg.path)).to.equal(chg.value);
+				seen = true;
+			});
+
+			scope.set("foo.bar", "baz");
+			expect(seen).to.be.ok;
+		});
+
+		it("observes empty path", function() {
+			var seen = false;
+			scope.observe("", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel(),
+					previousModel: scope.getModel(),
+					path: "",
+					type: "update",
+					value: "foo",
+					oldValue: { foo: "bar" }
+				});
+
+				seen = true;
+			});
+
+			scope.set([], "foo");
+			expect(seen).to.be.ok;
+		});
+
+		it("observes dynamic path: *", function() {
+			var seen = false;
+			scope.observe("*", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo"),
+					previousModel: scope.getModel("foo"),
+					path: "foo",
+					type: "update",
+					value: { bar: "baz" },
+					oldValue: "bar"
+				});
+
+				seen = true;
+			});
+
+			scope.set("foo", { bar: "baz" });
+			expect(seen).to.be.ok;
+		});
+
+		it("observes dynamic path: *.bar.baz", function() {
+			var seen = false;
+			scope.observe("*.bar.baz", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo.bar.baz"),
+					previousModel: scope.getModel("foo.bar.baz"),
+					path: "foo.bar.baz",
+					type: "add",
+					value: "buz",
+					oldValue: undefined
+				});
+
+				seen = true;
+			});
+
+			scope.set("foo.bar.baz", "buz");
+			expect(seen).to.be.ok;
+		});
+
+		it("observes dynamic path: foo.*.baz", function() {
+			var seen = false;
+			scope.observe("foo.*.baz", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo.bar.baz"),
+					previousModel: scope.getModel("foo.bar.baz"),
+					path: "foo.bar.baz",
+					type: "add",
+					value: "buz",
+					oldValue: undefined
+				});
+
+				seen = true;
+			});
+
+			scope.set("foo.bar.baz", "buz");
+			expect(seen).to.be.ok;
+		});
+
+		it("observes dynamic path: foo.bar.*", function() {
+			var seen = false;
+			scope.observe("foo.bar.*", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo.bar.baz"),
+					previousModel: scope.getModel("foo.bar.baz"),
+					path: "foo.bar.baz",
+					type: "add",
+					value: "buz",
+					oldValue: undefined
+				});
+
+				seen = true;
+			});
+
+			scope.set("foo.bar.baz", "buz");
+			expect(seen).to.be.ok;
+		});
+
+		it("observes dynamic path: **", function() {
+			var seen = false;
+			scope.set("foo", { bar: "baz" });
+
+			scope.observe("**", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo.bar"),
+					previousModel: scope.getModel("foo.bar"),
+					path: "foo.bar",
+					type: "update",
+					value: { baz: "buz" },
+					oldValue: "baz"
+				});
+
+				seen = true;
+			});
+
+			scope.set("foo.bar", { baz: "buz" });
+			expect(seen).to.be.ok;
+		});
+
+		it("observes dynamic path: **.baz", function() {
+			var seen = false;
+			
+			scope.observe("**.baz", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo.bar.baz"),
+					previousModel: scope.getModel("foo.bar.baz"),
+					path: "foo.bar.baz",
+					type: "add",
+					value: "buz",
+					oldValue: undefined
+				});
+
+				seen = true;
+			});
+
+			scope.set("foo.bar.baz", "buz");
+			expect(seen).to.be.ok;
+		});
+
+		it("observes dynamic path: foo.**.baz", function() {
+			var seen = false;
+			scope.observe("foo.**.baz", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo.bar.bun.baz"),
+					previousModel: scope.getModel("foo.bar.bun.baz"),
+					path: "foo.bar.bun.baz",
+					type: "add",
+					value: "buz",
+					oldValue: undefined
+				});
+
+				seen = true;
+			});
+
+			scope.set("foo.bar.bun.baz", "buz");
+			expect(seen).to.be.ok;
+		});
+
+		it("observes dynamic path: foo.**", function() {
+			var seen = false;
+			scope.set("foo.bar.baz", "buz");
+
+			scope.observe("foo.**", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo.bar.baz"),
+					previousModel: scope.getModel("foo.bar.baz"),
+					path: "foo.bar.baz",
+					type: "update",
+					value: "bun",
+					oldValue: "buz"
+				});
+
+				seen = true;
+			});
+
+			scope.set("foo.bar.baz", "bun");
+			expect(seen).to.be.ok;
+		});
+
+		it("observing path foo.** captures changes at path foo", function() {
+			var seen = false;
+			scope.observe("foo.**", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("foo"),
+					previousModel: scope.getModel("foo"),
+					path: "foo",
+					type: "update",
+					value: "buz",
+					oldValue: "bar"
+				});
+
+				seen = true;
+			});
+
+			scope.set("foo", "buz");
+			expect(seen).to.be.ok;
+		});
+
+		it("observes changes to fallback value", function() {
+			var seen = false;
+
+			scope.observe("bar", function(chg) {
+				expect(this).to.equal(scope);
+
+				expect(chg).to.deep.equal({
+					model: fallback.getModel("bar"),
+					previousModel: fallback.getModel("bar"),
+					path: "bar",
+					type: "update",
+					value: "bam",
+					oldValue: "baz"
+				});
+
+				seen = true;
+			});
+
+			fallback.set("bar", "bam");
+			expect(seen).to.be.ok;
+		});
+
+		it("observes changes to local value when the value was found in fallback", function() {
+			var seen = false;
+
+			scope.observe("bar", function(chg) {
+				expect(chg).to.deep.equal({
+					model: scope.getModel("bar"),
+					previousModel: fallback.getModel("bar"),
+					path: "bar",
+					type: "update",
+					value: "bam",
+					oldValue: "baz"
+				});
+
+				seen = true;
+			});
+
+			scope.set("bar", "bam");
+			expect(seen).to.be.ok;
+		});
+
+		it("doesn't observes changes to fallback when the value is in local model", function() {
+			scope.observe("foo", function(chg) {
+				throw new Error("A change was observed.");
+			});
+
+			fallback.set("foo", "bam");
+		});
+
+		it("observes unset on local value, falling back on a secondary value", function() {
+			var seen = false;
+			fallback.set("foo", "bug");
+
+			scope.observe("foo", function(chg) {
+				expect(chg).to.deep.equal({
+					model: fallback.getModel("foo"),
+					previousModel: scope.getModel("foo"),
+					path: "foo",
+					type: "update",
+					value: "bug",
+					oldValue: "bar"
+				});
+
+				seen = true;
+			});
+
+			scope.unset("foo");
+			expect(seen).to.be.ok;
+		});
 	});
 
 });
