d4048896324faee8ea737230e8d1ca63838d56b0
Tyler Johnson <tyler@vintyge.com>
2014-08-13 12:11:43 -0600

    updated tests for better insertBefore()

--- test/binding.js
+++ test/binding.js
@@ -47,19 +47,67 @@ describe("Bindings", function() {
 			}).to.throw(Error);
 		});
 
+		it("does nothing when adding a child if it is already at position in parent", function() {
+			var child = binding.appendChild(new Temple());
+
+			binding.on("child:remove", function(b) {
+				throw new Error("child was removed!");
+			});
+
+			binding.on("child:add", function(b) {
+				throw new Error("child was added!");
+			});
+
+			binding.on("child:move", function(b) {
+				throw new Error("child was moved!");
+			});
+
+			binding.appendChild(child);
+			expect(child.parent).to.equal(binding);
+		});
+
+		it("moves child if already in parent at a different location", function() {
+			var first = binding.appendChild(new Temple()),
+				child = binding.appendChild(new Temple()),
+				moved = false;
+
+			binding.on("child:remove", function(b) {
+				throw new Error("child was removed!");
+			});
+
+			binding.on("child:add", function(b) {
+				throw new Error("child was added!");
+			});
+
+			binding.on("child:move", function(b) {
+				if (child === b) moved = true;
+			});
+
+			binding.insertBefore(child, first);
+			expect(child.parent).to.equal(binding);
+			expect(moved).to.be.ok;
+		});
+
 		it("removes child binding", function() {
 			var child = binding.appendChild(new Temple());
 			binding.removeChild(child);
 			expect(binding.children).to.have.length(0);
 		});
 
-		it("removes child binding from exisiting parent before adding", function() {
+		it("removes child binding from existing parent before adding", function() {
 			var other = new Temple(),
 				child = new Temple(),
-				removed = false;
+				removed = false,
+				added = false;
 
 			other.on("child:remove", function(b) {
 				if (child === b) removed = true;
+				expect(added).to.not.be.ok;
+			});
+
+			binding.on("child:add", function(b) {
+				if (child === b) added = true;
+				expect(removed).to.be.ok;
 			});
 
 			other.appendChild(child);
@@ -68,6 +116,7 @@ describe("Bindings", function() {
 			expect(child.parent).to.equal(binding);
 			expect(removed).to.be.ok;
 		});
+
 
 		it("toString produces children HTML equivalent", function() {
 			binding.appendChild("Hello World");
