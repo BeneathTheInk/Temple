d8351eb10010330316b5d4cd0a8458ff7084df2c
Tyler Johnson <tyler@vintyge.com>
2014-08-15 09:41:21 -0600

    added build count to compiled source header

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,40 @@
+var count;
+
 module.exports = function(grunt) {
+
+	var refreshBuildCount = (function() {
+		var running = false;
+
+		return function() {
+			if (running) return;
+			running = true;
+			count = null;
+
+			require("child_process").exec("git rev-list HEAD --count", function(err, stdout, stderr) {
+				count = parseInt(stdout, 10);
+				if (isNaN(count) || count < 1) count = 0;
+				count += 1;
+				running = false;
+			});
+		}
+	})();
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		build_count: function() { return count; },
+		wait: {
+			build_count: {
+				options: {
+					delay: 500,
+					before: function() {
+						refreshBuildCount();
+					},
+					after: function() {
+						if (typeof count !== "number") return true;
+					}
+				}
+			}
+		},
 		clean: [ "dist/*.js" ],
 		browserify: {
 			dist: {
@@ -31,7 +64,7 @@ module.exports = function(grunt) {
 				src: 'dist/temple.dev.js',
 				dest: 'dist/temple.dev.js',
 				options: {
-					header: "/* Temple / (c) 2014 Beneath the Ink, Inc. / MIT License / Version <%= pkg.version %> */"
+					header: "/* Temple / (c) 2014 Beneath the Ink, Inc. / MIT License / Version <%= pkg.version %>, Build <%= build_count() %> */"
 				}
 			}
 		},
@@ -55,8 +88,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-wrap2000');
+	grunt.loadNpmTasks('grunt-wait');
 
-	grunt.registerTask('build-test', [ 'browserify:test', 'wrap2000:test' ]);
+	grunt.registerTask('build-test', [ 'wait', 'browserify:test', 'wrap2000:test' ]);
 	grunt.registerTask('build-dist', [ 'browserify:dist', 'wrap2000:dist', 'uglify:dist' ]);
 
 	grunt.registerTask('dist', [ 'clean', 'build-dist'  ]);
