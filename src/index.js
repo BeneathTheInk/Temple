da9b18d5e942de3a18ff60d99914262d5d5bcdab
Tyler Johnson <tyler@vintyge.com>
2015-07-16 12:18:57 -0600

    release 0.5.6

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var _ = require("underscore"),
 
 // properties that Node.js and the browser can handle
 var Temple = module.exports = _.defaults({
-	VERSION: "0.5.5",
+	VERSION: "0.5.6",
 	NODE_TYPE: NODE_TYPE,
 
 	// other parts
