9e07c5970ab1bf971d8859bd2d8a8b8aa7fa78aa
Tyler Johnson <tyler@tylerjohnson.me>
2015-09-11 13:16:08 -0600

    release 0.5.15

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var NODE_TYPE = require("./types");
 
 // properties that Node.js and the browser can handle
 var Temple = module.exports = {
-	VERSION: "0.5.14",
+	VERSION: "0.5.15",
 	NODE_TYPE: NODE_TYPE,
 
 	// other parts
