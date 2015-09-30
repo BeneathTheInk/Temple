6f67971ae526bc9fadf28e6e06bf6ebcbe11e631
Tyler Johnson <tyler@tylerjohnson.me>
2015-09-30 14:42:55 -0600

    cleaned up

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
-var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
@@ -13,6 +12,9 @@ var merge = require("merge-stream");
 var peg = require("gulp-peg");
 var rename = require("gulp-rename");
 var path = require("path");
+var pkg = require("./package.json");
+
+var copyright = "/*\n * Temple\n * (c) 2014-2015 Beneath the Ink, Inc.\n * MIT License\n * Version " + pkg.version + "\n */\n\n";
 
 gulp.task("clean", function() {
 	return del([ "lib/", "dist/" ]);
@@ -86,6 +88,7 @@ gulp.task("browser-dist", [ "compile" ], function() {
 		.pipe(source("temple.js"))
 		.pipe(buffer())
 		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(header(copyright))
 		.pipe(sourcemaps.write("."))
 		.pipe(gulp.dest('dist/'));
 });
@@ -96,18 +99,9 @@ gulp.task("browser-dist-min", [ "browser-dist" ], function() {
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(uglify())
 		.pipe(rename({ suffix: ".min" }))
+		.pipe(header(copyright))
 		.pipe(sourcemaps.write("."))
 		.pipe(gulp.dest("dist/"));
 });
 
-gulp.task("browser-dev", [ "compile" ], function buildScripts() {
-	var errors = errorHandler(true);
-
-	return js.bundle()
-		.on("error", errors)
-		.pipe(plumber({ errorHandler: errors }))
-		.pipe(source("temple.dev.js"))
-		.pipe(gulp.dest('dist/'));
-});
-
-gulp.task("default", [ "compile", "browser-dist", "browser-dist-min", "browser-dev" ]);
+gulp.task("default", [ "compile", "browser-dist", "browser-dist-min" ]);
