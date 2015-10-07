de130f19ebe7dad3414656d2b9638e870d453a5c
Tyler Johnson <tyler@tylerjohnson.me>
2015-10-07 14:29:28 -0600

    release 0.6.1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ if (typeof document !== "undefined") {
 	require("webcomponents.js/CustomElements.js");
 }
 
-export var version = "0.6.0";
+export var version = "0.6.1";
 
 import { compile, render, compileHTML, renderHTML } from "./compile";
 import { add as register, get, create } from "./globals";
