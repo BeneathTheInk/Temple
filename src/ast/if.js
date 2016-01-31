de0feeb3934d175b83f6349cab727d6be1745aea
Tyler Johnson <tyler@vintyge.com>
2016-01-30 17:02:47 -0700

    fixed section tags within attributes

--- src/ast/if.js
+++ src/ast/if.js
@@ -8,15 +8,29 @@ export default class If extends Node {
 	compile(data) {
 		this.start(data);
 
-		this.children.forEach((c, i) => {
-			let line = [];
-			line.push(!i ? "if" : !c.expression ? "else" : "else if", " ");
-			if (c.expression) line.push([ "(", c.expression.compile(data), ") " ]);
-			line.push("{");
-			this.write(line).indent();
-			this.push(c.compile(addKey(data, i.toString())));
-			this.outdent().write("}");
+		if (this.attribute) {
+			this.push("(");
+		}
+
+		this.children.forEach((c, i, l) => {
+			if (this.attribute) {
+				if (c.expression) this.push([ c.expression.compile(data), " ? " ]);
+				this.push([ "(", c.compile(addKey(data, i.toString())).join("+"), ")" ]);
+				if ((i + 1) === l.length && c.expression) this.push(" : ''");
+			} else {
+				let line = [];
+				line.push(!i ? "if" : !c.expression ? "else" : "else if", " ");
+				if (c.expression) line.push([ "(", c.expression.compile(data), ") " ]);
+				line.push("{");
+				this.write(line).indent();
+				this.push(c.compile(addKey(data, i.toString())));
+				this.outdent().write("}");
+			}
 		});
+
+		if (this.attribute) {
+			this.push(")");
+		}
 
 		return this.end();
 	}
