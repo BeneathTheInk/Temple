de26f95027f212d20190d35c37dd975441434326
Tyler Johnson <tyler@vintyge.com>
2016-01-31 23:01:33 -0700

    release 0.7.0-alpha.2

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import * as plugins from "./plugins";
 import * as decorators from "./plugins/decorators";
 import * as actions from "./plugins/actions";
 
-export var version = "0.7.0-alpha1";
+export var version = "0.7.0-alpha.2";
 
 export * from "./templates";
 export * from "./compile";
