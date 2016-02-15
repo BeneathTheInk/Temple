3e461157b9bc1e43eb04eefea7ae57c0d2894fd8
Tyler Johnson <tyler@vintyge.com>
2016-02-14 17:30:32 -0700

    fix: moved cli compile to utils

--- src/commands/compile.js
+++ src/commands/compile.js
@@ -1,31 +1,9 @@
 import fs from "fs-promise";
 import path from "path";
-import {fromPairs} from "lodash";
-
-function fetchFile(f) {
-	return fs.readFile(path.resolve(f), {
-		encoding: "utf-8"
-	}).then(src => {
-		return [f, src];
-	});
-}
-
-function fetchStdin() {
-	return new Promise((resolve, reject) => {
-		let src = "";
-		process.stdin.setEncoding("utf-8");
-		process.stdin.on("data", (d) => src += d);
-		process.stdin.on("end", () => resolve(["_input.js",src]));
-		process.stdin.on("error", reject);
-	});
-}
+import {compile, panic} from "./utils";
 
 export default function(argv, Temple) {
-	let p = argv._.map(fetchFile);
-	if (!process.stdin.isTTY) p.push(fetchStdin());
-
-	return Promise.all(p).then(r => {
-		let result = Temple.compile(fromPairs(r));
+	return compile(Temple, argv._).then(result => {
 		let mapFile = argv["source-map"];
 		let output = argv.output ? path.resolve(argv.output) : null;
 		let p = [];
@@ -36,7 +14,7 @@ export default function(argv, Temple) {
 		} else {
 			if (mapFile === "inline") mapFile = null;
 			code = result.toString(mapFile);
-			
+
 			if (typeof mapFile === "string") {
 				if (output) mapFile = path.resolve(path.dirname(output), mapFile);
 				else mapFile = path.resolve(mapFile);
@@ -45,12 +23,8 @@ export default function(argv, Temple) {
 		}
 
 		if (output) return fs.writeFile(output, code);
-		else {
-			console.log(code);
-			process.exit(0);
-		}
-	}).catch(e => {
-		console.error(e.stack || e);
-		process.exit(1);
-	});
+
+		console.log(code);
+		process.exit(0);
+	}).catch(panic);
 }
