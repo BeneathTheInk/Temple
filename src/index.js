06cbc9a4a4c693816336ef183227b46c73f372fe
Tyler Johnson <tyler@vintyge.com>
2016-02-16 00:19:50 -0700

    fix: upgraded trackr-objects

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import * as idom from "./core/idom";
 import * as utils from "./core/utils";
 import Trackr from "trackr";
-import { Map, List, Variable } from "trackr-objects";
 import Context from "./core/context";
 import * as AST from "./ast";
 import pkg from "../package.json";
@@ -13,11 +12,12 @@ import * as actions from "./plugins/actions";
 
 export const version = pkg.version || "edge";
 
+export { Map, List, Variable, trackProperty } from "trackr-objects";
 export * from "./core/templates";
 export * from "./core/compile";
 export * from "./core/builtins";
 
 export {
-	Context, idom, utils, Map, List, Variable,
-	stylesheet,	plugins, decorators, actions, AST, Trackr
+	Trackr, Context, AST, idom, utils,
+	stylesheet, plugins, decorators, actions
 };
