dc823e4602cadd77ca6f92ac091c73a26cfc1840
Tyler Johnson <tyler@vintyge.com>
2016-02-15 23:41:25 -0700

    fix: removed script indentation

--- src/ast/script.js
+++ src/ast/script.js
@@ -1,6 +1,5 @@
 import Node from "./node";
 import {SourceNode} from "source-map";
-import detectIndent from "detect-indent";
 
 export default class Script extends Node {
 	get reactive() { return true; }
@@ -11,18 +10,9 @@ export default class Script extends Node {
 
 		let line = this._line || 1;
 		let column = this._column || 1;
-		let indent = detectIndent(this.value);
 
 		this.value.split(/\r?\n/g).forEach((l, i) => {
-			let lindent = detectIndent(l);
-			l = l.replace(/^\s+/, "");
-			let len = Math.round(lindent.amount / indent.amount);
-			let tabs = this.tabs();
-			let tabchar = data.tabchar;
-			if (tabchar == null) tabchar = "  ";
-			for (let j = 0; j < len; j++) tabs += tabchar;
-
-			this.push(new SourceNode(i + line, i === 0 ? column : 1, data.originalFilename, tabs + l + "\n"));
+			this.push(new SourceNode(i + line, i === 0 ? column : 1, data.originalFilename, l + "\n"));
 		});
 
 		return this.end();
